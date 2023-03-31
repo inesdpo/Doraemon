689603b281ea21199c72156c207a815073e1d595
francescobonetti <francesco2.bonetti@mail.polimi.it>
2023-03-31 13:56:19 +0100

    Updated to the lastest changes and moved the gitignore

---
+++ Prototype/Assets/GridSnap.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSnap : MonoBehaviour
+{   
+    //[SerialiedField] public GameObject object ;
+    public Vector3 boxPos =  Vector3.zero; 
+ 
+    float width = 0.0f;
+    float height = 0.0f;
+
+    Vector3 topLeftCorner = Vector3.zero;
+
+    float gridUnitWidth = 0.0f;
+    float gridUnitHeight= 0.0f;
+
+    Vector3 gridUnitCenter = Vector3.zero;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BoxCollider boxCollider = GetComponent<BoxCollider>();
+
+
+        boxPos = transform.position;
+
+        width = boxCollider.size.x;
+        height = boxCollider.size.z;
+        
+        topLeftCorner = new Vector3( boxPos.x - width/2.0f , boxPos.y , boxPos.z - height/2.0f);
+
+        
+
+
+        gridUnitWidth = width / 5.0f;
+        gridUnitHeight= height / 8.0f;
+
+        gridUnitCenter = new Vector3( topLeftCorner.x + gridUnitWidth/2.0f , topLeftCorner.y , topLeftCorner.z + gridUnitHeight/2.0f );
+
+        //GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        //sphere.transform.position = gridUnitCenter;
+
+        for(int x = 0; x < 5; x++) 
+        {
+            for(int z = 0; z < 8; z++) 
+            {
+                Vector3 gridSnapPoint = new Vector3(gridUnitCenter.x + x * gridUnitWidth , gridUnitCenter.y , gridUnitCenter.z + z * gridUnitHeight);
+                
+                GameObject mySphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                mySphere.transform.position = gridSnapPoint;
+                //Debug.Log(mySphere.transform);
+                mySphere.transform.scale = new Vector3(0.2f, 0.2f, 0.2f);
+            }
+        }
+    }
+        
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
