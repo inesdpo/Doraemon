b69c9c5aa806059bf632bcced0b73620ff89e4b0
inesdpo <117300107+inesdpo@users.noreply.github.com>
2023-06-07 11:19:59 +0100

    Feedback Script

---
+++ Prototype/Assets/Scripts/FeedbackPopup.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static Unity.VisualScripting.Metadata;
+
+public class FeedbackPopup : MonoBehaviour
+{
+    //public Image imageComponent;
+    public GameObject spriteBad;
+    public GameObject spriteGood;
+    public GameObject spriteNormal;
+    public Transform[] children;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //children = gameObject.GetComponentsInChildren<Transform>(); ;
+
+
+        Transform parentTransform = transform;
+        int children = parentTransform.childCount;
+
+        Debug.Log(children);
+
+
+        if (children >= 6)
+        {
+            spriteGood.SetActive(true);
+            spriteBad.SetActive(false);
+            spriteNormal.SetActive(false);
+        }
+        else if (children <= 3)
+        {
+            spriteBad.SetActive(true);
+            spriteGood.SetActive(false);
+            spriteNormal.SetActive(false);
+        }
+        else
+        {
+            spriteNormal.SetActive(true);
+            spriteBad.SetActive(false);
+            spriteGood.SetActive(false);
+        }
+    }
+}
